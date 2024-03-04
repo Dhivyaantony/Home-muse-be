86563423ca8c97f0a563ead7572aab59569f95b2
Dhivya <dhivyaantony778@gmail.com>
2024-03-04 16:33:57 +0530

    bcrypt

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Middleware setup
-app.use(cors({origin:[`https://home-muse-client.onrender.com/`,`http://localhost:3000`]}));
+app.use(cors({origin:[`https://home-muse-client.onrender.com/`,`http://localhost:5000`]}));
 app.use(logger('dev'));
 
 app.use(express.json());
