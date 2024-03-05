cbe465504c911366736060d42c602625aac65e7d
Dhivya <dhivyaantony778@gmail.com>
2024-03-05 14:04:20 +0530

    port

--- app.js
+++ app.js
@@ -64,13 +64,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-// Define the port
-const PORT = process.env.PORT || 5000;
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 // Export the Express app
 module.exports = app;
